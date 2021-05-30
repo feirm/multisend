6cf63e6ac08a29637e52a8f40cf766e5a88e54e6
Jack Coble <jack@coble.uk>
2021-05-30 13:39:09 +0100

    Start of Multisend contract

---
+++ contracts/Multisend.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
+
+contract Multisend {
+    using SafeMath for uint256;
+}
